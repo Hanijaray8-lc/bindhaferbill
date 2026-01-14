996b2597344dc2ee35586871822b7bc0c32a142f
Hanijaray8-lc <hanija.lc240073@gmail.com>
2026-01-14 16:38:45 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use('/uploads', express.static('uploads')); // serve uploaded images
+
+// MongoDB connection
+mongoose.connect('mongodb+srv://alphanewprojectlc:CEoMY7QsrBIJokk3@aizeldb.nuoy3.mongodb.net/Bindhafer?retryWrites=true&w=majority', {
+
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err));
+
+// Routes
+const InvoiceRoutes = require('./routes/InvoiceRoutes');
+const creditRoutes = require('./routes/creditroute');
+
+app.use('/api/invoice', InvoiceRoutes);
+app.use('/api/credit', creditRoutes);
+
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
